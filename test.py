f4f3429d157988d4823f20d5155b951f8471fb1b
Paul J. Davis <paul.joseph.davis@gmail.com>
2009-12-03 02:16:19 -0500

    Fix test app

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
 
-from gunicorn.httpserver import WSGIServer
-
-
-
-
 def app(environ, start_response):
     """Simplest possible application object"""
     data = 'Hello, World!\n'
@@ -15,6 +10,3 @@ def app(environ, start_response):
     start_response(status, response_headers)
     return [data]
 
-if __name__ == '__main__':
-    server = WSGIServer(("127.0.0.1", 8000), 1, simple_app)
-    server.run()
