b20a0ace1eef1370c424ebae78ca9ef7cfafbeb1
Paul J. Davis <paul.joseph.davis@gmail.com>
2010-02-22 11:17:00 -0500

    Added wsgiref validator to the example.

--- examples/test.py
+++ examples/test.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -
 
+from wsgiref.validate import validator
+
+@validator
 def app(environ, start_response):
     """Simplest possible application object"""
     data = 'Hello, World!\n'
     status = '200 OK'
     response_headers = [
         ('Content-type','text/plain'),
-        ('Content-Length', len(data))
+        ('Content-Length', str(len(data)))
     ]
     start_response(status, response_headers)
     return [data]
-
