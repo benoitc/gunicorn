562c9a2d70eba1f535556fbd60cf4834440f1942
benoitc <bchesneau@gmail.com>
2010-03-13 23:25:29 +0100

    revert previous change

--- gunicorn/sock.py
+++ gunicorn/sock.py
@@ -33,10 +33,11 @@ class BaseSocket(object):
     
     def set_options(self, sock, bound=False):
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.setblocking(0)
         if not bound:
             self.bind(sock)
+        sock.setblocking(0)
         sock.listen(self.conf['backlog'])
+        
         return sock
         
     def bind(self, sock):
