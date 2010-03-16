06d58ae1c43c7f95b80e0aabfa6ae56bf8cfbb17
benoitc <benoitc@pollen.nymphormation.org>
2010-03-16 22:15:32 +0100

    oups revert this change in util

--- gunicorn/util.py
+++ gunicorn/util.py
@@ -101,8 +101,9 @@ def get_maxfd():
     return maxfd
 
 def close_on_exec(fd):
-    flags = fcntl.fcntl(fd, fcntl.F_GETFD) | fcntl.FD_CLOEXEC
-    fcntl.fcntl(fd, fcntl.F_SETFL, flags)
+    flags = fcntl.fcntl(fd, fcntl.F_GETFD)
+    flags |= fcntl.FD_CLOEXEC
+    fcntl.fcntl(fd, fcntl.F_SETFD, flags)
     
 def set_non_blocking(fd):
     flags = fcntl.fcntl(fd, fcntl.F_GETFL) | os.O_NONBLOCK
