c8154ea5fdf9430c4d2516f621420899c58b7827
Paul J. Davis <paul.joseph.davis@gmail.com>
2010-05-06 18:26:10 -0400

    Simplify the gtornado monkey patch a bit.

--- gunicorn/workers/gtornado.py
+++ gunicorn/workers/gtornado.py
@@ -15,25 +15,17 @@ from tornado.wsgi import WSGIContainer
 from gunicorn.workers.base import Worker
 from gunicorn import __version__ as gversion
 
-
-def patch_request_handler():
-    web = sys.modules.pop("tornado.web")
-
-    old_clear = web.RequestHandler.clear
-
-    def clear(self):
-        old_clear(self)
-        self._headers["Server"] += " (Gunicorn/%s)" % gversion
-         
-    web.RequestHandler.clear = clear
-    sys.modules["tornado.web"] = web
-    
-
 class TornadoWorker(Worker):
     
     @classmethod
     def setup(cls):
-        patch_request_handler()
+        web = sys.modules.pop("tornado.web")
+        old_clear = web.RequestHandler.clear
+        def clear(self):
+            old_clear(self)
+            self._headers["Server"] += " (Gunicorn/%s)" % gversion
+        web.RequestHandler.clear = clear
+        sys.modules["tornado.web"] = web
         
     def watchdog(self):
         self.notify()
