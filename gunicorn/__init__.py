bc2d0112a8e10615d9fcd05727f44b72e9658e5a
benoitc <bchesneau@gmail.com>
2010-11-10 19:28:18 +0100

    use only git shorttag with 'develop' command, also don'ty load it each time. spotted by @davisp .

--- gunicorn/__init__.py
+++ gunicorn/__init__.py
@@ -3,26 +3,6 @@
 # This file is part of gunicorn released under the MIT license. 
 # See the NOTICE for more information.
 
-
-import os
-
-if os.environ.get('release') != "true":
-
-    minor_tag = ""
-    try:
-        from gunicorn.util import popen3
-
-        stdin, stdout, stderr = popen3("git rev-parse --short HEAD --")
-        error = stderr.read()
-        if not error:
-            git_tag = stdout.read()[:-1]
-            minor_tag = ".%s-git" % git_tag
-    except OSError:        
-        pass
-else:
-    minor_tag = ""
-    
-
-version_info = (0, 11, "2%s" % minor_tag)
+version_info = (0, 11, 2)
 __version__ = ".".join(map(str, version_info))
 SERVER_SOFTWARE = "gunicorn/%s" % __version__
