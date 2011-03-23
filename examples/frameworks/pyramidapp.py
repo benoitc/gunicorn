374bb7bfff681fe23e0cf4cf6c376507f23a6944
benoitc <benoitc@gmail.com>
2011-03-23 07:32:37 +0100

    add pyramid app example

---
+++ examples/frameworks/pyramidapp.py
@@ -0,0 +1,15 @@
+
+from pyramid.config import Configurator
+from pyramid.response import Response
+from paste.httpserver import serve
+
+def hello_world(request):
+    return Response('Hello world!')
+
+def goodbye_world(request):
+    return Response('Goodbye world!')
+
+config = Configurator()
+config.add_view(hello_world)
+config.add_view(goodbye_world, name='goodbye')
+app = config.make_wsgi_app()
