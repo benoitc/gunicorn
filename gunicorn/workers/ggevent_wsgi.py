78e3d08f960c3337c6ef9030cea916dd9a5870d3
benoitc <bchesneau@gmail.com>
2011-05-11 17:16:29 +0200

    add missing GeventWSGIWorker

--- gunicorn/workers/ggevent_wsgi.py
+++ gunicorn/workers/ggevent_wsgi.py
@@ -3,8 +3,7 @@
 # This file is part of gunicorn released under the MIT license. 
 # See the NOTICE for more information.
 
-from __future__ import with_statement
-from gunicorn.worker.ggevent import *
+from gunicorn.worker.ggevent import BASE_WSGI_ENV, GeventBaseWorker 
 from gevent import wsgi
 
 
@@ -19,4 +18,9 @@ class WSGIHandler(wsgi.WSGIHandler):
         
         
 class WSGIServer(wsgi.WSGIServer):
-    base_env = BASE_WSGI_ENV        
+    base_env = BASE_WSGI_ENV  
+
+class GeventWSGIWorker(GeventBaseWorker):
+    "The Gevent StreamServer based workers."
+    server_class = WSGIServer
+    wsgi_handler = WSGIHandler
