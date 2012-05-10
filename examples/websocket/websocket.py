d94e494f122033ef22e511c17b42032be1fdfd48
CMGS <ilskdw@mspil.edu.cn>
2012-05-10 18:37:40 +0800

    bug fix

--- examples/websocket/websocket.py
+++ examples/websocket/websocket.py
@@ -10,6 +10,7 @@ import struct
 import logging
 from socket import error as SocketError
 
+import eventlet
 from gunicorn.workers.async import ALREADY_HANDLED
 from eventlet import pools
 
@@ -409,3 +410,37 @@ class WebSocket(object):
         self._send_closing_frame()
         self.socket.shutdown(True)
         self.socket.close()
+
+# demo app
+import os
+import random
+def handle(ws):
+    """  This is the websocket handler function.  Note that we 
+    can dispatch based on path in here, too."""
+    if ws.path == '/echo':
+        while True:
+            m = ws.wait()
+            if m is None:
+                break
+            ws.send(m)
+            
+    elif ws.path == '/data':
+        for i in xrange(10000):
+            ws.send("0 %s %s\n" % (i, random.random()))
+            eventlet.sleep(0.1)
+                            
+wsapp = WebSocketWSGI(handle)
+def app(environ, start_response):
+    """ This resolves to the web page or the websocket depending on
+    the path."""
+    if environ['PATH_INFO'] == '/' or environ['PATH_INFO'] == "":
+        data = open(os.path.join(
+                     os.path.dirname(__file__), 
+                     'websocket.html')).read()
+        data = data % environ
+        start_response('200 OK', [('Content-Type', 'text/html'),
+                                 ('Content-Length', len(data))])
+        return [data]
+    else:
+        return wsapp(environ, start_response)
+
