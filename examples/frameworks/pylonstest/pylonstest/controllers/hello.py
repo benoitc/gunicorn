f4ddf0f7a7efacea83d853857bc5a0b7dff397d9
benoitc <bchesneau@gmail.com>
2012-05-15 10:11:48 +0200

    fix pylons example

--- examples/frameworks/pylonstest/pylonstest/controllers/hello.py
+++ examples/frameworks/pylonstest/pylonstest/controllers/hello.py
@@ -1,7 +1,7 @@
 import logging
 
 from pylons import request, response, session, tmpl_context as c
-from pylons.controllers.util import abort, redirect_to
+from pylons.controllers.util import abort
 
 from pylonstest.lib.base import BaseController, render
 
