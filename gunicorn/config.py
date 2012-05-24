d79ff999ce895e2ed0ea02aa8729e6da736dfc27
benoitc <bchesneau@gmail.com>
2012-05-24 12:13:34 +0200

    fix multiple issues with request limit
    patch from Djoume Salvetti . address the following issues in gunicorn:
    
    * Gunicorn does not limit the size of a request header (the
    * limit_request_field_size configuration parameter is not used)
    
    * When the configured request limit is lower than its maximum value, the
    * maximum value is used instead. For instance if limit_request_line is
    * set to 1024, gunicorn will only limit the request line to 4096 chars
    * (this issue also affects limit_request_fields)
    
    * Request limits are not limited to their maximum authorized values. For
    * instance it is possible to set limit_request_line to 64K (this issue
    * also affects limit_request_fields)
    
    * Setting limit_request_fields and limit_request_field_size to 0 does
    * not make them unlimited. The following patch allows limit_request_line
    * and limit_request_field_size to be unlimited. limit_request_fields can
    * no longer be unlimited (I can't imagine 32K fields to not be enough
    * but I have a use case where 8K for the request line is not enough).
    
    * Parsing errors (premature client disconnection) are not reported
    
    * When request line limit is exceeded the configured value is reported
    * instead of the effective value.

--- gunicorn/config.py
+++ gunicorn/config.py
@@ -449,8 +449,8 @@ class LimitRequestLine(Setting):
         restriction on the length of a request-URI allowed for a request
         on the server. A server needs this value to be large enough to
         hold any of its resource names, including any information that
-        might be passed in the query part of a GET request. By default
-        this value is 4094 and can't be larger than 8190.
+        might be passed in the query part of a GET request. Value is a number
+        from 0 (unlimited) to 8190.
 
         This parameter can be used to prevent any DDOS attack.
         """
@@ -466,10 +466,10 @@ class LimitRequestFields(Setting):
     desc= """\
         Limit the number of HTTP headers fields in a request.
 
-        Value is a number from 0 (unlimited) to 32768. This parameter is
-        used to limit the number of headers in a request to prevent DDOS
-        attack. Used with the `limit_request_field_size` it allows more
-        safety.
+        This parameter is used to limit the number of headers in a request to
+        prevent DDOS attack. Used with the `limit_request_field_size` it allows
+        more safety. By default this value is 100 and can't be larger than
+        32768.
         """
 
 class LimitRequestFieldSize(Setting):
