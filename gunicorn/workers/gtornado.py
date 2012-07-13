4d70a526f2f4ddd0c8d708b3439684b6822ce550
benoitc <bchesneau@gmail.com>
2012-07-13 18:13:19 +0200

    fix typo

--- gunicorn/workers/gtornado.py
+++ gunicorn/workers/gtornado.py
@@ -60,7 +60,7 @@ class TornadoWorker(Worker):
         # instance of tornado.wsgi.WSGIApplication
         app = self.wsgi
         if not isinstance(app, tornado.web.Application) or \
-           isinstance(app, tornado.wsgi.WSGIApplicaion):
+           isinstance(app, tornado.wsgi.WSGIApplication):
             app = WSGIContainer(app)
 
         # Monkey-patching HTTPConnection.finish to count the
