3118b08b99d01d8c7a98ddc729165d3c26f57119
benoitc <bchesneau@gmail.com>
2012-12-14 10:32:58 +0100

    fix display when using ssl

--- gunicorn/sock.py
+++ gunicorn/sock.py
@@ -55,7 +55,13 @@ class TCPSocket(BaseSocket):
     FAMILY = socket.AF_INET
 
     def __str__(self):
-        return "http://%s:%d" % self.sock.getsockname()
+        if self.conf.is_ssl:
+            scheme = "https"
+        else:
+            scheme = "http"
+
+        addr =  self.sock.getsockname()
+        return "%s://%s:%d" % (scheme, addr[0], addr[1])
 
     def set_options(self, sock, bound=False):
         sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
