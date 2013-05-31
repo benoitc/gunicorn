b614c44416225121350bef4f6cdcf044798b8632
Moriyoshi Koizumi <mozo@mozo.jp>
2013-05-31 17:46:22 +0900

    bump version.

--- gunicorn/__init__.py
+++ gunicorn/__init__.py
@@ -4,5 +4,5 @@
 # See the NOTICE for more information.
 
 version_info = (0, 17, 4)
-__version__ = ".".join([str(v) for v in version_info])
+__version__ = ".".join([str(v) for v in version_info]) + 'moriyoshi0'
 SERVER_SOFTWARE = "gunicorn/%s" % __version__
