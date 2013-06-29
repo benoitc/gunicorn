benoitc/gunicorn

commit 423e203db944ea5b250e2816cbeb9c4741cc736f

Fix to read the settings module specified by --settings or django_settings parameter

gunicorn/app/wsgiapp.py
@@ -8,6 +8,7 @@ import sys
 
 from gunicorn import util
 from gunicorn.app.base import Application
+from gunicorn.app import djangoapp
 
 
 class WSGIApplication(Application):
@@ -24,6 +25,7 @@ class WSGIApplication(Application):
         sys.path.insert(0, cwd)
 
     def load(self):
+        djangoapp.make_default_env(self.cfg)
         return util.import_app(self.app_uri)
 
 

commit cd90653fe3275621c2d8311175c49b9638c4d908

fix non django application loading
fix error introduced in 423e203db944ea5b250e2816cbeb9c4741cc736f.

gunicorn/app/wsgiapp.py
@@ -25,7 +25,11 @@ class WSGIApplication(Application):
         sys.path.insert(0, cwd)
 
     def load(self):
-        djangoapp.make_default_env(self.cfg)
+        try:
+            djangoapp.make_default_env(self.cfg)
+        except RuntimeError:
+            # ignore silently error while loading non django apps.
+            pass
         return util.import_app(self.app_uri)
 
 