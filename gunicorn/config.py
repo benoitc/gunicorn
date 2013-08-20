0b16936c2ececb675f8364226d8a7ffd6607501e
Alexis Lê-Quôc <alq@datadoghq.com>
2013-08-19 20:07:07 -0400

    Add statsd configuration parameter
    Not yet connected to anything.

--- gunicorn/config.py
+++ gunicorn/config.py
@@ -367,6 +367,14 @@ def validate_post_request(val):
     else:
         raise TypeError("Value must have an arity of: 4")
 
+def validate_hostport(val):
+    val = validate_string(val)
+    elements = val.split(":")
+    if len(elements) == 1:
+        raise TypeError("Value must consist of: hostname:port")
+    elif len(elements) == 2:
+        return (elements[0], int(elements[1]))    
+
 def get_default_config_file():
     config_path = os.path.join(os.path.abspath(os.getcwd()), 'gunicorn.conf.py')
     if os.path.exists(config_path):
@@ -1330,4 +1338,15 @@ class CertFile(Setting):
     default = None
     desc = """\
     SSL certificate file
+    """
+
+class StatsdHost(Setting):
+    name = "statsd_host"
+    section = "Instrumentation"
+    cli = ["--statsd"]
+    meta = "STRING"
+    validator = validate_hostport
+    default = "localhost:8125"
+    desc = """\
+    Host and port of the statsD server to send metrics to
     """
