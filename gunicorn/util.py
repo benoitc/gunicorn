5c78adf9b91ba299ddbf513716b7db9240ee0d4a
Berker Peksag <berker.peksag@gmail.com>
2014-02-14 02:26:09 +0200

    Use email.utils.formatdate in gunicorn.util.http_date.
    There is a also an open issue for CPython:
    
    http://bugs.python.org/issue747320

--- gunicorn/util.py
+++ gunicorn/util.py
@@ -4,6 +4,7 @@
 # See the NOTICE for more information.
 
 
+import email.utils
 import fcntl
 import io
 import os
@@ -33,11 +34,6 @@ timeout_default = object()
 CHUNK_SIZE = (16 * 1024)
 
 MAX_BODY = 1024 * 132
-
-weekdayname = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-monthname = [None,
-             'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-             'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 # Server and Date aren't technically hop-by-hop
 # headers, but they are in the purview of the
@@ -400,11 +396,7 @@ def http_date(timestamp=None):
     """Return the current date and time formatted for a message header."""
     if timestamp is None:
         timestamp = time.time()
-    year, month, day, hh, mm, ss, wd, y, z = time.gmtime(timestamp)
-    s = "%s, %02d %3s %4d %02d:%02d:%02d GMT" % (
-            weekdayname[wd],
-            day, monthname[month], year,
-            hh, mm, ss)
+    s = email.utils.formatdate(timestamp, localtime=False, usegmt=True)
     return s
 
 
