f1fc2710f894156c8c0af5b7bec02dbc5c149810
Yue Du <ifduyue@gmail.com>
2014-05-14 14:08:18 +0800

    Remove unused imports

--- gunicorn/reloader.py
+++ gunicorn/reloader.py
@@ -5,7 +5,6 @@
 
 import os
 import re
-import signal
 import sys
 import time
 import threading
--- gunicorn/workers/base.py
+++ gunicorn/workers/base.py
@@ -7,7 +7,6 @@ from datetime import datetime
 import os
 import signal
 import sys
-import traceback
 
 
 from gunicorn import util
