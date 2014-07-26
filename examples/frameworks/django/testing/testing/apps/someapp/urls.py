43b8687ceb38716aa94dc4eec07404895e34e715
benoitc <bchesneau@gmail.com>
2014-07-26 10:23:11 +0200

    fix django ex

--- examples/frameworks/django/testing/testing/apps/someapp/urls.py
+++ examples/frameworks/django/testing/testing/apps/someapp/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^acsv$', 'testing.apps.someapp.views.acsv'),
