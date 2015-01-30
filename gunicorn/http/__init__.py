f22267a1bed6520a1531f33b30130da556dd1348
Berker Peksag <berker.peksag@gmail.com>
2015-01-30 22:30:56 +0200

    Fix gunicorn.http.__all__.
    All parameters should be a str type.

--- gunicorn/http/__init__.py
+++ gunicorn/http/__init__.py
@@ -6,4 +6,4 @@
 from gunicorn.http.message import Message, Request
 from gunicorn.http.parser import RequestParser
 
-__all__ = [Message, Request, RequestParser]
+__all__ = ['Message', 'Request', 'RequestParser']
