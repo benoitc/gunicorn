2af94689408db24e4d94c79ec0a7693d60fbc21f
Berker Peksag <berker.peksag@gmail.com>
2015-07-03 07:41:24 +0300

    Parametrize valid request tests.

--- tests/test_001-valid-requests.py
+++ tests/test_001-valid-requests.py
@@ -3,30 +3,25 @@
 # This file is part of gunicorn released under the MIT license.
 # See the NOTICE for more information.
 
-import treq
-
 import glob
 import os
+
+import pytest
+
+import treq
+
 dirname = os.path.dirname(__file__)
-
 reqdir = os.path.join(dirname, "requests", "valid")
+httpfiles = glob.glob(os.path.join(reqdir, "*.http"))
 
-def a_case(fname):
+
+@pytest.mark.parametrize("fname", httpfiles)
+def test_http_parser(fname):
     env = treq.load_py(os.path.splitext(fname)[0] + ".py")
+
     expect = env['request']
     cfg = env['cfg']
     req = treq.request(fname, expect)
+
     for case in req.gen_cases(cfg):
         case[0](*case[1:])
-
-def test_http_parser():
-    for fname in glob.glob(os.path.join(reqdir, "*.http")):
-        if os.getenv("GUNS_BLAZING"):
-            env = treq.load_py(os.path.splitext(fname)[0] + ".py")
-            expect = env['request']
-            cfg = env['cfg']
-            req = treq.request(fname, expect)
-            for case in req.gen_cases(cfg):
-                yield case
-        else:
-            yield (a_case, fname)
