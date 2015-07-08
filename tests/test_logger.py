da37bcfebed8f54ba92f51d0f0b3b1dcd803b449
Berker Peksag <berker.peksag@gmail.com>
2015-07-08 12:16:09 +0300

    Adda test case for #1069.

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,7 +1,5 @@
 import datetime
 
-import t
-
 from gunicorn.config import Config
 from gunicorn.glogging import Logger
 
@@ -24,3 +22,19 @@ def test_atoms_defaults():
     assert atoms['r'] == 'GET http://my.uri HTTP/1.1'
     assert atoms['{accept}i'] == 'application/json'
     assert atoms['{content-type}o'] == 'application/json'
+
+
+def test_get_username_from_basic_auth_header():
+    request = SimpleNamespace(headers=())
+    response = SimpleNamespace(
+        status='200', response_length=1024, sent=1024,
+        headers=(('Content-Type', 'text/plain'),),
+    )
+    environ = {
+        'REQUEST_METHOD': 'GET', 'RAW_URI': 'http://my.uri',
+        'SERVER_PROTOCOL': 'HTTP/1.1',
+        'HTTP_AUTHORIZATION': 'Basic YnJrMHY6',
+    }
+    logger = Logger(Config())
+    atoms = logger.atoms(response, request, environ, datetime.timedelta(seconds=1))
+    assert atoms['u'] == 'brk0v'
