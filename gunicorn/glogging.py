fe156b755224160056c85a88075743ca86f89819
Mat Moore <mwehtam@gmail.com>
2015-09-05 16:16:20 +0100

    The logconfig_dict setting is not available on 2.6

--- gunicorn/glogging.py
+++ gunicorn/glogging.py
@@ -7,7 +7,12 @@ import base64
 import time
 import logging
 logging.Logger.manager.emittedNoHandlerWarning = 1
-from logging.config import fileConfig, dictConfig
+from logging.config import fileConfig
+try:
+    from logging.config import dictConfig
+except ImportError:
+    # python 2.6
+    dictConfig = None
 import os
 import socket
 import sys
@@ -199,7 +204,7 @@ class Logger(object):
                 self.access_log, cfg, self.syslog_fmt, "access"
             )
 
-        if cfg.logconfig_dict:
+        if dictConfig and cfg.logconfig_dict:
             config = CONFIG_DEFAULTS.copy()
             config.update(cfg.logconfig_dict)
             dictConfig(config)
