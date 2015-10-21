7dd8a53c8c6afbc4199ff9b7fb2d6c3068fd9d9a
y.umezaki <bungoume3@yahoo.co.jp>
2015-10-21 22:48:39 +0900

    Add accesslog params
    Fix KeyError
    
    Update access logger tests
    
    Update settings.rst docs

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -13,13 +13,20 @@ def test_atoms_defaults():
     )
     request = SimpleNamespace(headers=(('Accept', 'application/json'),))
     environ = {
-        'REQUEST_METHOD': 'GET', 'RAW_URI': 'http://my.uri',
+        'REQUEST_METHOD': 'GET', 'RAW_URI': '/my/path?foo=bar',
+        'PATH_INFO': '/my/path', 'QUERY_STRING': 'foo=bar',
         'SERVER_PROTOCOL': 'HTTP/1.1',
     }
     logger = Logger(Config())
     atoms = logger.atoms(response, request, environ, datetime.timedelta(seconds=1))
     assert isinstance(atoms, dict)
-    assert atoms['r'] == 'GET http://my.uri HTTP/1.1'
+    assert atoms['r'] == 'GET /my/path?foo=bar HTTP/1.1'
+    assert atoms['m'] == 'GET'
+    assert atoms['U'] == '/my/path'
+    assert atoms['q'] == 'foo=bar'
+    assert atoms['H'] == 'HTTP/1.1'
+    assert atoms['b'] == '1024'
+    assert atoms['B'] == 1024
     assert atoms['{accept}i'] == 'application/json'
     assert atoms['{content-type}o'] == 'application/json'
 
@@ -31,7 +38,8 @@ def test_get_username_from_basic_auth_header():
         headers=(('Content-Type', 'text/plain'),),
     )
     environ = {
-        'REQUEST_METHOD': 'GET', 'RAW_URI': 'http://my.uri',
+        'REQUEST_METHOD': 'GET', 'RAW_URI': '/my/path?foo=bar',
+        'PATH_INFO': '/my/path', 'QUERY_STRING': 'foo=bar',
         'SERVER_PROTOCOL': 'HTTP/1.1',
         'HTTP_AUTHORIZATION': 'Basic YnJrMHY6',
     }
