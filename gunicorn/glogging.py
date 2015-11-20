38261b523560caa71ce35d65750dd6ceae3f41c5
benoitc <bchesneau@gmail.com>
2015-11-20 11:13:44 +0100

    check if the auth type is Basic before getting the user name
    fix #1148

--- gunicorn/glogging.py
+++ gunicorn/glogging.py
@@ -381,7 +381,7 @@ class Logger(object):
     def _get_user(self, environ):
         user = None
         http_auth = environ.get("HTTP_AUTHORIZATION")
-        if http_auth:
+        if http_auth and http_auth.startswith('Basic'):
             auth = http_auth.split(" ", 1)
             if len(auth) == 2:
                 try:
