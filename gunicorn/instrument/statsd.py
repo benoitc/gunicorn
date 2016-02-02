353508cd8a5e0bbb4ad9a4ef01ff456a49f43eee
Krzysztof Urbaniak <urban@fail.pl>
2016-02-03 00:55:58 +0100

    fix access logging in gaiohttp worker

--- gunicorn/instrument/statsd.py
+++ gunicorn/instrument/statsd.py
@@ -93,9 +93,12 @@ class Statsd(Logger):
         """
         Logger.access(self, resp, req, environ, request_time)
         duration_in_ms = request_time.seconds * 1000 + float(request_time.microseconds) / 10 ** 3
+        status = resp.status
+        if isinstance(status, str):
+            status = int(status.split(None, 1)[0])
         self.histogram("gunicorn.request.duration", duration_in_ms)
         self.increment("gunicorn.requests", 1)
-        self.increment("gunicorn.request.status.%d" % int(resp.status.split()[0]), 1)
+        self.increment("gunicorn.request.status.%d" % status, 1)
 
     # statsD methods
     # you can use those directly if you want
