9d158bec94c91c62cd5cc28a27268a5b8804d795
Phil Jones <philip.graham.jones@googlemail.com>
2016-05-03 12:15:36 +0100

    Add access_log_format to config example (#1251)
    The example given is the current default log format.

--- examples/example_config.py
+++ examples/example_config.py
@@ -147,6 +147,7 @@ tmp_upload_dir = None
 errorlog = '-'
 loglevel = 'info'
 accesslog = '-'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
 #
 # Process naming
