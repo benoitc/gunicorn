062b48d8d218b51484adcc35a18ec762aedb1e63
benoitc <bchesneau@gmail.com>
2016-05-15 02:20:13 +0200

    lockfile improvements
    - rename LockFile.lock  to acquire
    - rename LockFile.unlock to release
    - move the lockfile management in sepate functions inside the arbiter
    - remove the "closed" argument from the socket.close method and add a new "destroy" function that will be called whent  the socket can be unlinked (cal release)
    - fix tests

--- tests/test_lockfile.py
+++ tests/test_lockfile.py
@@ -6,10 +6,10 @@ from gunicorn.util import tmpfile
 def test_lockfile():
     lockname = tmpfile(prefix="gunicorn-tests", suffix=".lock")
     lock_file = LockFile(lockname)
-    assert lock_file.locked() == False
+    assert lock_file.released() == True
     assert os.path.exists(lockname)
-    lock_file.lock()
-    assert lock_file.locked() == True
-    lock_file.unlock()
-    assert lock_file.locked() == False
+    lock_file.acquire()
+    assert lock_file.released() == False
+    lock_file.release()
+    assert lock_file.released() == True
     assert os.path.exists(lockname) == False
