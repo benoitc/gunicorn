b4dc48dc0265c192ca3c677476923b1cdcaa25d2
Benoit Chesneau <bchesneau@gmail.com>
2016-05-20 15:55:55 +0200

    Revert "lockfile improvements"

--- tests/test_sock.py
+++ tests/test_sock.py
@@ -11,8 +11,17 @@ from gunicorn import sock
 @mock.patch('socket.fromfd')
 def test_unix_socket_close_delete_if_exlock(fromfd, unlink, getpid):
     gsock = sock.UnixSocket('test.sock', mock.Mock(), mock.Mock(), mock.Mock())
-    gsock.destroy()
+    gsock.close(False)
     unlink.assert_called_with('test.sock')
+
+
+@mock.patch('os.getpid')
+@mock.patch('os.unlink')
+@mock.patch('socket.fromfd')
+def test_unix_socket_close_keep_if_no_exlock(fromfd, unlink, getpid):
+    gsock = sock.UnixSocket('test.sock', mock.Mock(), mock.Mock(), mock.Mock())
+    gsock.close(True)
+    unlink.assert_not_called()
 
 
 @mock.patch('os.getpid')
@@ -23,5 +32,5 @@ def test_unix_socket_not_deleted_by_worker(fromfd, unlink, getpid):
     gsock = sock.UnixSocket('test.sock', mock.Mock(), mock.Mock(), fd)
     getpid.reset_mock()
     getpid.return_value = "fake"  # fake a pid change
-    gsock.destroy()
+    gsock.close(False)
     unlink.assert_not_called()
