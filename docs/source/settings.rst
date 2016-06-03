8e3e545321c62a1dced99c1fe115a89b78511ef5
Vivian Ho <vho@lyft.com>
2016-06-03 14:30:37 -0700

    updated docs

--- docs/source/settings.rst
+++ docs/source/settings.rst
@@ -471,31 +471,30 @@ access_log_format
 
 The access log format.
 
-===========  ===========
-Identifier   Description
-===========  ===========
-h            remote address
-l            ``'-'``
-u            user name
-t            date of the request
-r            status line (e.g. ``GET / HTTP/1.1``)
-m            request method
-U            URL path without query string
-q            query string
-H            protocol
-s            status
-B            response length
-b            response length or ``'-'`` (CLF format)
-f            referer
-a            user agent
-T            request time in seconds
-D            request time in microseconds
-L            request time in decimal seconds
-p            process ID
-{Header}i    request header
-{Header}o    response header
-{Variable}e  environment variable
-===========  ===========
+==========  ===========
+Identifier  Description
+==========  ===========
+h           remote address
+l           ``'-'``
+u           user name
+t           date of the request
+r           status line (e.g. ``GET / HTTP/1.1``)
+m           request method
+U           URL path without query string
+q           query string
+H           protocol
+s           status
+B           response length
+b           response length or ``'-'`` (CLF format)
+f           referer
+a           user agent
+T           request time in seconds
+D           request time in microseconds
+L           request time in decimal seconds
+p           process ID
+{Header}i   request header
+{Header}o   response header
+==========  ===========
 
 errorlog
 ~~~~~~~~
--- gunicorn/config.py
+++ gunicorn/config.py
@@ -1074,30 +1074,31 @@ class AccessLogFormat(Setting):
     desc = """\
         The access log format.
 
-        ==========  ===========
-        Identifier  Description
-        ==========  ===========
-        h           remote address
-        l           ``'-'``
-        u           user name
-        t           date of the request
-        r           status line (e.g. ``GET / HTTP/1.1``)
-        m           request method
-        U           URL path without query string
-        q           query string
-        H           protocol
-        s           status
-        B           response length
-        b           response length or ``'-'`` (CLF format)
-        f           referer
-        a           user agent
-        T           request time in seconds
-        D           request time in microseconds
-        L           request time in decimal seconds
-        p           process ID
-        {Header}i   request header
-        {Header}o   response header
-        ==========  ===========
+        ===========  ===========
+        Identifier   Description
+        ===========  ===========
+        h            remote address
+        l            ``'-'``
+        u            user name
+        t            date of the request
+        r            status line (e.g. ``GET / HTTP/1.1``)
+        m            request method
+        U            URL path without query string
+        q            query string
+        H            protocol
+        s            status
+        B            response length
+        b            response length or ``'-'`` (CLF format)
+        f            referer
+        a            user agent
+        T            request time in seconds
+        D            request time in microseconds
+        L            request time in decimal seconds
+        p            process ID
+        {Header}i    request header
+        {Header}o    response header
+        {Variable}e  environment variable
+        ===========  ===========
         """
 
 
