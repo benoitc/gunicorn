b4f38a50e3da395b06aeed295e9d613baf104431
Patrick Browne <pt.browne@gmail.com>
2016-08-23 13:28:18 +0200

    Update util.py

--- gunicorn/util.py
+++ gunicorn/util.py
@@ -388,8 +388,7 @@ def import_app(module):
         app = eval(obj, mod.__dict__)
     except NameError:
         exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback,
-                                  limit=3, file=sys.stdout)
+        traceback.print_exception(exc_type, exc_value, exc_traceback)
         raise AppImportError("Failed to find application: %r" % module)
 
     if app is None:
