38d6dbf0692bdf9e0f22756b8d930041d5989c55
ViCky <vicky.chawda@gmail.com>
2016-09-17 15:38:14 +0530

    Removed additional lines

--- examples/frameworks/cherryapp.py
+++ examples/frameworks/cherryapp.py
@@ -1,14 +1,8 @@
 import cherrypy
 
-cherrypy.config.update({'environment': 'embedded'})
-
 class Root(object):
     @cherrypy.expose
     def index(self):
         return 'Hello World!'
 
-cherrypy.config.update({'engine.autoreload.on': False})
-cherrypy.server.unsubscribe()
-cherrypy.engine.start()
 app = cherrypy.tree.mount(Root())
-
