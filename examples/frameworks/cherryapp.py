f65baef904ab0e3d79cf32a276498e21abab9f7d
ViCky <vicky.chawda@gmail.com>
2016-09-17 15:41:14 +0530

    Moved line 3 above - cherrypy.config.update

--- examples/frameworks/cherryapp.py
+++ examples/frameworks/cherryapp.py
@@ -1,8 +1,13 @@
 import cherrypy
 
+cherrypy.config.update({'environment': 'embedded'})
+
 class Root(object):
     @cherrypy.expose
     def index(self):
         return 'Hello World!'
 
+app = cherrypy.tree.mount(Root())
+cherrypy.server.unsubscribe()
+cherrypy.engine.start()
 app = cherrypy.tree.mount(Root())
