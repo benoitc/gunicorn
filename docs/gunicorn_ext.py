0589a54789c2923eb6b9565e96ed38accb8e3045
Berker Peksag <berker.peksag@gmail.com>
2017-01-13 12:08:17 +0300

    Add versionadded

--- docs/gunicorn_ext.py
+++ docs/gunicorn_ext.py
@@ -25,6 +25,8 @@ for reference on setting at the command line.
     For example, to specify the bind address and number of workers::
 
         $ GUNICORN_CMD_ARGS="--bind=127.0.0.1 --workers=3" gunicorn app:app
+        
+    .. versionadded:: 19.7
 """
 ISSUE_URI = 'https://github.com/benoitc/gunicorn/issues/%s'
 PULL_REQUEST_URI = 'https://github.com/benoitc/gunicorn/pull/%s'
