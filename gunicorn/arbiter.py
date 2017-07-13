benoitc/gunicorn

commit 2407dd29a6b44e96150d48ac12d0d16be2506725

select.error is a subclass of OSError since Python 3.3

gunicorn/arbiter.py
@@ -362,11 +362,10 @@ class Arbiter(object):
                 return
             while os.read(self.PIPE[0], 1):
                 pass
-        except select.error as e:
-            if e.args[0] not in [errno.EAGAIN, errno.EINTR]:
-                raise
-        except OSError as e:
-            if e.errno not in [errno.EAGAIN, errno.EINTR]:
+        except (select.error, OSError) as e:
+            # TODO: select.error is a subclass of OSError since Python 3.3.
+            errno = getattr(e, 'errno', e.args[0])
+            if errno not in [errno.EAGAIN, errno.EINTR]:
                 raise
         except KeyboardInterrupt:
             sys.exit()

commit 56ef7710dd3e0b1343f3f157e2afb10a1237395d

Fix a silly error introduced by 2407dd29a6b44e96150d48ac12d0d16be2506725

gunicorn/arbiter.py
@@ -364,8 +364,8 @@ class Arbiter(object):
                 pass
         except (select.error, OSError) as e:
             # TODO: select.error is a subclass of OSError since Python 3.3.
-            errno = getattr(e, 'errno', e.args[0])
-            if errno not in [errno.EAGAIN, errno.EINTR]:
+            error_number = getattr(e, 'errno', e.args[0])
+            if error_number not in [errno.EAGAIN, errno.EINTR]:
                 raise
         except KeyboardInterrupt:
             sys.exit()