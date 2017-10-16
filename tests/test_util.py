f877f0eadfd96b3616aa7b16ff6136a6bbdc9122
Berker Peksag <berker.peksag@gmail.com>
2017-10-16 08:57:29 +0300

    Update and rename test_utils.py to test_util.py

--- tests/test_util.py
+++ tests/test_util.py
@@ -7,6 +7,6 @@ from gunicorn import util
 
 
 def test_parse_address():
-    # test unix socket address
+    # Test unix socket addresses (PR #1623)
     assert util.parse_address('unix://var/run/test.sock') == 'var/run/test.sock'
     assert util.parse_address('unix:/var/run/test.sock') == '/var/run/test.sock'
