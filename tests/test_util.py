f9fd1d6f345e9141db987014ab4a183ce92db258
Yury Petrov <ypetrov@yandex-team.ru>
2017-12-16 19:42:24 +0500

    test for util.split_request_uri

--- tests/test_util.py
+++ tests/test_util.py
@@ -7,6 +7,7 @@ import pytest
 
 from gunicorn import util
 from gunicorn.errors import AppImportError
+from gunicorn.six.moves.urllib.parse import SplitResult
 
 
 @pytest.mark.parametrize('test_input, expected', [
@@ -69,3 +70,19 @@ def test_to_bytestring():
         util.to_bytestring(100)
     msg = '100 is not a string'
     assert msg in str(err)
+
+
+@pytest.mark.parametrize('test_input, expected', [
+    ('https://example.org/a/b?c=1#d',
+     SplitResult(scheme='https', netloc='example.org', path='/a/b', query='c=1', fragment='d')),
+    ('a/b?c=1#d',
+     SplitResult(scheme='', netloc='', path='a/b', query='c=1', fragment='d')),
+    ('/a/b?c=1#d',
+     SplitResult(scheme='', netloc='', path='/a/b', query='c=1', fragment='d')),
+    ('//a/b?c=1#d',
+     SplitResult(scheme='', netloc='', path='//a/b', query='c=1', fragment='d')),
+    ('///a/b?c=1#d',
+     SplitResult(scheme='', netloc='', path='///a/b', query='c=1', fragment='d')),
+])
+def test_split_request_uri(test_input, expected):
+    assert util.split_request_uri(test_input) == expected
