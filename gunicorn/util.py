5358ac71695d984a8568bf40051125f728029ed6
Yury Petrov <ypetrov@yandex-team.ru>
2017-12-21 00:36:00 +0500

    comment for urlsplit fix

--- gunicorn/util.py
+++ gunicorn/util.py
@@ -550,6 +550,7 @@ def split_request_uri(uri):
         # When the path starts with //, urlsplit considers it as a
         # relative uri while the RFC says we should consider it as abs_path
         # http://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html#sec5.1.2
+        # We use temporary dot prefix to workaround this behaviour
         parts = _compat.urlsplit("." + uri)
         return parts._replace(path=parts.path[1:])
 
