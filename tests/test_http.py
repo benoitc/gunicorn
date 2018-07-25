5b1ddb8d9bb4cebcf7a2ac95b9b0ce6558263cea
Hugo <hugovk@users.noreply.github.com>
2018-07-25 14:29:46 +0300

    Upgrade Python syntax with pyupgrade --py3-plus

--- tests/test_http.py
+++ tests/test_http.py
@@ -82,7 +82,7 @@ def test_http_header_encoding():
     response = Response(mocked_request, mocked_socket, None)
 
     # set umlaut header
-    response.headers.append(('foo', u'häder'))
+    response.headers.append(('foo', 'häder'))
     with pytest.raises(UnicodeEncodeError):
         response.send_headers()
 
