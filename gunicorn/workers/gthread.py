3b6b704981c402637b756ecc887fb80c246b5a56
hugovk <hugovk@users.noreply.github.com>
2018-07-27 07:47:22 +0300

    Raise more descriptive message if selectors module not found

--- gunicorn/workers/gthread.py
+++ gunicorn/workers/gthread.py
@@ -36,8 +36,18 @@ except ImportError:
     Python version.
     """)
 
-import selectors
-
+try:
+    # Python 3.4+
+    import selectors
+except ImportError:
+    # Python 2
+    try:
+        import selectors34 as selectors
+    except ImportError:
+        raise RuntimeError(
+            "You need to install the 'selectors34' package to use this worker "
+            "with this Python version."
+        )
 
 class TConn(object):
 
