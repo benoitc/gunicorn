5d72755c96621856d1fa609c5878c356936ec52b
Benoit Chesneau <bchesneau@gmail.com>
2019-01-24 23:15:19 +0100

    make sure all examples work on python 3
    fix #1961
    
    (cherry picked from commit fe7632fe3738dfcc1aaf5c2fc7ced3a3c7f42214)

--- examples/echo.py
+++ examples/echo.py
@@ -24,8 +24,7 @@ def app(environ, start_response):
     response_headers = [
         ('Content-type', 'text/plain'),
         ('Content-Length', str(len(data))),
-        ('X-Gunicorn-Version', __version__),
-        ("Test", "test тест"),
+        ('X-Gunicorn-Version', __version__)
     ]
     start_response(status, response_headers)
     return iter([data])
