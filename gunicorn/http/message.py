265e58f9e82f64e003015fb25823f5a983be8c27
benoitc <bchesneau@gmail.com>
2019-11-20 22:46:00 +0100

    fix wsgi.input_terminated
    Sometimes both TRANSFER-ENCODING=chunked and CONTENT_LENGTH are set. Since gunicorn prioritise (following the HTTP1.1 spec) chunked encoding we should make sure in this case to signal to th application that the input is terminated by the server.
    
    Without the change gunicorn were always setting wsg.input_terminated to False when a CONTENT_LENGTH header was present ignoring that Gunicorn was afaik handling the termination.

--- gunicorn/http/message.py
+++ gunicorn/http/message.py
@@ -33,6 +33,7 @@ class Message(object):
         self.version = None
         self.headers = []
         self.trailers = []
+        self.terminated = True
         self.body = None
         self.scheme = "https" if cfg.is_ssl else "http"
 
@@ -151,6 +152,7 @@ class Message(object):
             if content_length < 0:
                 raise InvalidHeader("CONTENT-LENGTH", req=self)
 
+            self.terminated = False
             self.body = Body(LengthReader(self.unreader, content_length))
         else:
             self.body = Body(EOFReader(self.unreader))
