e150ca4ff87d52b1c2b3d5376fc71789b65a3fb4
Benoit Chesneau <bchesneau@gmail.com>
2019-11-21 11:53:13 +0100

    add "*.so.*" case to util.find_lib
    The actual libc name in musl is: libc.musl-x86_64.so.1 this case should handle it.

--- gunicorn/util.py
+++ gunicorn/util.py
@@ -659,7 +659,7 @@ def _findWalk_ldpath(name):
         if _is_elf(f):
             return os.path.basename(f)
         prefix = os.path.join(d, 'lib'+name)
-        for suffix in ['so', 'so.*']:
+        for suffix in ['so', 'so.*', '*.so.*']:
             for f in glob('{0}.{1}'.format(prefix, suffix)):
                 if _is_elf(f):
                     return os.path.basename(f)
