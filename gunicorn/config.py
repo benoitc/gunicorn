2cd0c10897ee180183211d43403123d38c3b44ee
Anmar Zubaydi <>
2019-12-12 14:05:12 -0500

    Add milliseconds option to request_time in access_log

--- gunicorn/config.py
+++ gunicorn/config.py
@@ -1266,6 +1266,7 @@ class AccessLogFormat(Setting):
         f            referer
         a            user agent
         T            request time in seconds
+        M            request time in milliseconds
         D            request time in microseconds
         L            request time in decimal seconds
         p            process ID
