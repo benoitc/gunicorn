595aa44c87a0984946516989b2678283249a48f0
nanaones <nara03050@gmail.com>
2020-07-29 21:44:42 +0900

    refactor: delete FRAMEWORK_VERSION and FRAMEWORK environments
    Users can record the framework and version they use in requirements file that records the package dependencies pointed to by the $REQUIRENMENTS_FILE_PATH environment variable.

--- examples/docker/config/install.sh
+++ examples/docker/config/install.sh
@@ -1,11 +1,5 @@
 #! /bin/bash
 
-if [ -n "$FRAMEWORK_VERSION" ] && [ -n "$FRAMEWORK" ]; then
-  pip3 install "$FRAMEWORK"=="$FRAMEWORK_VERSION"
-elif [ -n "$FRAMEWORK" ]; then
-    pip3 install "$FRAMEWORK"
-fi
-
 if [ -n "$REQUIRENMENTS_FILE_PATH" ]; then
   pip3 install -r "$REQUIRENMENTS_FILE_PATH"
 fi
