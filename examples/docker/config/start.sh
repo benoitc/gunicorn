eb17d50c37cf8be447a6948e075f940fd7780314
nanaones <nara03050@gmail.com>
2020-12-19 23:50:21 +0900

    refact: delete echo -e

--- examples/docker/config/start.sh
+++ examples/docker/config/start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-pip3 install gunicorn &&\
+pip3 install gunicorn
+
 if [ -n "$REQUIRENMENTS_FILE_PATH" ]; then
   pip install -r "$REQUIRENMENTS_FILE_PATH"
-fi &&\
+fi
+
 if [ -n "$CONFIG_FILE_PATH" ]; then
-  gunicorn -c `echo -e "$CONFIG_FILE_PATH"` `echo -e "$START_OPTION"`
-fi &&\
-gunicorn `echo -e "$START_OPTION"`
+  gunicorn -c $CONFIG_FILE_PATH $START_OPTION
+fi
+
+gunicorn $START_OPTION
+
+
