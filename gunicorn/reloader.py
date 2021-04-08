9c78f7e58b020845b99e94a1ba0ee133f2f8dbd4
laggardkernel <laggardkernel@gmail.com>
2021-04-08 13:53:34 +0800

    Fix __init__ for dummy InotifyReloader
    To raise `ImportError` when InotifyReloader is not available,
    init params for the dummy InotifyReloader should be the same
    as params in the real one.

--- gunicorn/reloader.py
+++ gunicorn/reloader.py
@@ -118,7 +118,7 @@ if has_inotify:
 else:
 
     class InotifyReloader(object):
-        def __init__(self, callback=None):
+        def __init__(self, extra_files=None, callback=None):
             raise ImportError('You must have the inotify module installed to '
                               'use the inotify reloader')
 
