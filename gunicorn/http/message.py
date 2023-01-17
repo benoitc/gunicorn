30baeebf337850a7ce3cb8099ffa248cef5831ef
benoitc <bchesneau@gmail.com>
2023-01-17 21:53:23 +0100

    Revert "get address from tuple"
    This reverts commit c9d01c801967596f66636cfc0c47660339d32631.

--- gunicorn/http/message.py
+++ gunicorn/http/message.py
@@ -31,7 +31,7 @@ class Message(object):
         self.cfg = cfg
         self.unreader = unreader
         self.peer_addr = peer_addr
-        self.remote_addr = peer_addr[0]
+        self.remote_addr = peer_addr
         self.version = None
         self.headers = []
         self.trailers = []
