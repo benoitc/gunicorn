4ce82358e88357982676153c0141236d32c1d36b
Jelmer Draaijer <info@jelmert.nl>
2023-10-31 08:42:48 +0100

    Add Python 3.12 to test matrix and add classifiers

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
     'Programming Language :: Python :: 3 :: Only',
     'Programming Language :: Python :: Implementation :: CPython',
     'Programming Language :: Python :: Implementation :: PyPy',
