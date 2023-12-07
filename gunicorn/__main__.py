c3396b9786b07b10d71d2b224888c7cd32be3625
Paul J. Dorn <pajod@users.noreply.github.com>
2023-12-07 10:38:30 +0100

    github actions: cache and test run_module (-m)

--- gunicorn/__main__.py
+++ gunicorn/__main__.py
@@ -4,4 +4,8 @@
 # See the NOTICE for more information.
 
 from gunicorn.app.wsgiapp import run
-run()
+
+if __name__ == "__main__":
+    # see config.py - argparse defaults to basename(argv[0]) == "__main__.py"
+    # todo: let runpy.run_module take care of argv[0] rewriting
+    run(prog="gunicorn")
