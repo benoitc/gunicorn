6b1c15b69d4faacb54d92fd07d22e02b3363f2fa
Paul J. Dorn <pajod@users.noreply.github.com>
2023-12-08 01:29:54 +0100

    skip eventlet, not yet supported oon python 3.12
    will work again, but should still be reverten when stdlib conflict resolved in eventlet

--- tests/workers/test_geventlet.py
+++ tests/workers/test_geventlet.py
@@ -3,5 +3,15 @@
 # This file is part of gunicorn released under the MIT license.
 # See the NOTICE for more information.
 
+import pytest
+import sys
+
 def test_import():
+
+    try:
+        import eventlet
+    except AttributeError:
+        if sys.version_info >= (3, 12):
+            pytest.skip("Ignoring eventlet failures on Python 3.12")
+        raise
     __import__('gunicorn.workers.geventlet')
