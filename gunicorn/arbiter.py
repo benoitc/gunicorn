e65abbd23d744334e47059fa68e6f994b40d96a8
Richard Eklycke <richard@eklycke.se>
2024-02-02 11:28:54 +0100

    arbiter: Handle SIGCHLD in normal/main process context
    ... as opposed to in signal context. This is beneficial, since it
    means that we can, in a signal safe way, print messages about why
    e.g. a worker stopped its execution.
    
    And since handle_sigchld() logs what it does anyway, don't bother
    printing out that we're handling SIGCHLD. If workers are killed at
    rapid pace, we won't get as many SIGCHLD as there are workers killed
    anyway.

--- gunicorn/arbiter.py
+++ gunicorn/arbiter.py
@@ -41,12 +41,9 @@ class Arbiter(object):
 
     # I love dynamic languages
     SIG_QUEUE = []
-    SIGNALS = [getattr(signal, "SIG%s" % x)
-               for x in "HUP QUIT INT TERM TTIN TTOU USR1 USR2 WINCH".split()]
-    SIG_NAMES = dict(
-        (getattr(signal, name), name[3:].lower()) for name in dir(signal)
-        if name[:3] == "SIG" and name[3] != "_"
-    )
+    SIGNALS = [getattr(signal.Signals, "SIG%s" % x)
+               for x in "CHLD HUP QUIT INT TERM TTIN TTOU USR1 USR2 WINCH".split()]
+    SIG_NAMES = dict((sig, sig.name[3:].lower()) for sig in SIGNALS)
 
     def __init__(self, app):
         os.environ["SERVER_SOFTWARE"] = SERVER_SOFTWARE
@@ -186,7 +183,6 @@ class Arbiter(object):
         # initialize all signals
         for s in self.SIGNALS:
             signal.signal(s, self.signal)
-        signal.signal(signal.SIGCHLD, self.handle_chld)
 
     def signal(self, sig, frame):
         if len(self.SIG_QUEUE) < 5:
@@ -220,7 +216,8 @@ class Arbiter(object):
                 if not handler:
                     self.log.error("Unhandled signal: %s", signame)
                     continue
-                self.log.info("Handling signal: %s", signame)
+                if sig != signal.SIGCHLD:
+                    self.log.info("Handling signal: %s", signame)
                 handler()
                 self.wakeup()
         except (StopIteration, KeyboardInterrupt):
@@ -237,10 +234,9 @@ class Arbiter(object):
                 self.pidfile.unlink()
             sys.exit(-1)
 
-    def handle_chld(self, sig, frame):
+    def handle_chld(self):
         "SIGCHLD handling"
         self.reap_workers()
-        self.wakeup()
 
     def handle_hup(self):
         """\
@@ -392,7 +388,10 @@ class Arbiter(object):
         # instruct the workers to exit
         self.kill_workers(sig)
         # wait until the graceful timeout
-        while self.WORKERS and time.time() < limit:
+        while True:
+            self.reap_workers()
+            if not self.WORKERS or time.time() >= limit:
+                break
             time.sleep(0.1)
 
         self.kill_workers(signal.SIGKILL)
