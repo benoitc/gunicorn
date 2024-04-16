a77e269910d9bc7d6096e7dd9117f650e2853978
benoitc <bchesneau@gmail.com>
2024-04-17 00:36:19 +0200

    bump to 22.0

--- gunicorn/__init__.py
+++ gunicorn/__init__.py
@@ -3,7 +3,7 @@
 # This file is part of gunicorn released under the MIT license.
 # See the NOTICE for more information.
 
-version_info = (21, 2, 0)
+version_info = (22, 0, 0)
 __version__ = ".".join([str(v) for v in version_info])
 SERVER = "gunicorn"
 SERVER_SOFTWARE = "%s/%s" % (SERVER, __version__)
