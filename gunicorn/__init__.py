375e79e95b78f81b70af0c1ae8e32b7f4beee273
Benoit Chesneau <bchesneau@gmail.com>
2026-01-24 02:13:42 +0100

    release: bump version to 24.1.1

--- gunicorn/__init__.py
+++ gunicorn/__init__.py
@@ -2,7 +2,7 @@
 # This file is part of gunicorn released under the MIT license.
 # See the NOTICE for more information.
 
-version_info = (24, 1, 0)
+version_info = (24, 1, 1)
 __version__ = ".".join([str(v) for v in version_info])
 SERVER = "gunicorn"
 SERVER_SOFTWARE = "%s/%s" % (SERVER, __version__)
