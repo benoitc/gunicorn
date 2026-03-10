1f19d3f5b2b59daca81042d58b08b89544cae8fe
Paul J. Dorn <pajod@users.noreply.github.com>
2026-03-10 22:53:18 +0100

    asgi: get valid/invalid http tests to pass
    * change asgi HTTP error exceptions to match base sync worker
    * add more unicode test cases (requests/valid/027.http lacked query arg)
    * be explicit about config where tests/test_asgi.py::MockConfig non-default

--- tests/requests/invalid/006.py
+++ tests/requests/invalid/006.py
@@ -2,5 +2,9 @@
 # This file is part of gunicorn released under the MIT license.
 # See the NOTICE for more information.
 
+from gunicorn.config import Config
 from gunicorn.http.errors import LimitRequestLine
+
+cfg = Config()
+cfg.set("limit_request_line", 4094)
 request = LimitRequestLine
