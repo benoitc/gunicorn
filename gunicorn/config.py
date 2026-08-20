45c49b0b6169dfc60516a2762b8faa6ba8fd5fb7
Inada Naoki <songofacandy@gmail.com>
2026-08-20 17:17:05 +0900

    fix comment

--- gunicorn/config.py
+++ gunicorn/config.py
@@ -2,7 +2,8 @@
 # This file is part of gunicorn released under the MIT license.
 # See the NOTICE for more information.
 
-# Please remember to run "make -C docs html" after update "desc" attributes.
+# Please remember to run "python scripts/build_settings_doc.py"
+# after update "desc" attributes.
 
 import argparse
 import copy
